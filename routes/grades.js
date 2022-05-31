a2627e67d39c34c87379cd195e0d2b64665cc0f7
rackeelBrooks <84402686+cout-Rackeel@users.noreply.github.com>
2022-05-31 15:59:16 -0500

    made changes to averages table

--- routes/grades.js
+++ routes/grades.js
@@ -3,45 +3,58 @@ var router = express.Router();
 var conn = require('../lib/db');
 
 router.get('/' , (req,res,next) => {
-  var average ;
 
-  conn.query('SELECT AVG(gr.grade) AS avg FROM grades.grades gr' , (err,avg) => {
-    var avg1 = avg;
-    console.log(avg);
-    var gradeSQL = "SELECT s.f_name , s.l_name , s.class ,  sb.subject , t.term , gr.grade , lt.letter" +
+    var gradeSQL = "SELECT gr.sub_id ,  gr.stu_id , s.f_name , s.l_name , s.class ,  sb.subject , t.term , gr.grade , lt.letter" +
   " FROM grades.students s , grades.subjects sb , grades.terms t , grades.grades gr , grades.letter_grades lt" +
-  " WHERE gr.stu_id = s.id AND gr.sub_id = sb.id AND gr.term_id = t.id AND gr.letter_id = lt.id;"
+  " WHERE gr.stu_id = s.id AND gr.sub_id = sb.id  AND gr.sub_id = 1 AND gr.term_id = t.id AND gr.letter_id = lt.id;"
 
   conn.query( gradeSQL , (err,rows) => {
     if (err) throw err 
     var locals = {
       title : 'GradesWeb -  Grades List Page',
       grades : rows,
+      sub : req.body.subject
     }
     res.render('grades/grades',locals);
   })
 })
     
-  })
-
-
-  
 
 router.post('/' , (req,res,next) => {
 
-  var gradeSQL = "SELECT s.f_name , s.l_name , s.class ,  sb.subject , t.term , gr.grade , lt.letter" +
+  var gradeSQL = "SELECT gr.sub_id , gr.stu_id , s.f_name , s.l_name , s.class ,  sb.subject , t.term , gr.grade , lt.letter" +
   " FROM grades.students s , grades.subjects sb , grades.terms t , grades.grades gr , grades.letter_grades lt" +
   " WHERE gr.stu_id = s.id AND gr.sub_id = sb.id AND gr.term_id = t.id AND gr.letter_id = lt.id" +
-  " AND sb.subject LIKE '"+ req.body.subject +"%'"
+  " AND sb.id LIKE " + req.body.subject
 
   conn.query(gradeSQL , (err,rows) => {
     if (err) throw err
     var locals = {
       title : 'GradesWeb -  Grades List Page',
-      grades : rows
+      grades : rows,
+      sub : req.body.subject
     }
     res.render('grades/grades',locals);
   })
+})
+
+router.get('/show-avg/:id/:sub' , (req,res,next) => {
+
+  avgSQL = "SELECT gr.stu_id , s.f_name , s.l_name , s.class ,  sb.subject , AVG(gr.grade) AS avg , lt.letter" +
+  " FROM grades.students s , grades.subjects sb , grades.terms t , grades.grades gr , grades.letter_grades lt" +
+  " WHERE gr.stu_id = s.id AND gr.sub_id = sb.id  AND gr.term_id = t.id AND gr.letter_id = lt.id" +
+  " AND gr.sub_id LIKE " + req.params.sub +
+  " AND  gr.stu_id LIKE " + req.params.id;
+
+  conn.query(avgSQL , (err,rows) => {
+    if(err) throw err;
+    var locals = {
+      title : 'GradesWeb - Average List Page',
+      data : rows
+    }
+    res.render('grades/show-avg' , locals)
+  })
+
 })
 
 
